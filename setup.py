1f6db7f15fa03c2d94eb5b89a0b60df7850b5866
Mike Thornton <mthornton@Underrock.local>
2016-02-04 18:23:48 -0600

    Use setuptools

--- setup.py
+++ setup.py
@@ -1,32 +1,27 @@
-from distutils.core import setup
+from setuptools import setup
 
 
-def main():
-    setup(
-        name='testing.elasticsearch',
-        packages=['testing'],
-        package_dir={'': 'src'},
-        version=open('VERSION.txt').read().strip(),
-        author='Jon Gartman',
-        author_email='jongartman@gmail.com',
-        url='https://github.com/jong/testing.elasticsearch',
-        keywords=['testing', 'elasticsearch'],
-        license='MIT',
-        description='Automatically sets up an elasticsearch instance in a temporary directory, and destroys it after testing',
-        classifiers=[
-            "Programming Language :: Python",
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: MIT License",
-            "Natural Language :: English",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-        long_description=open('README.rst').read(),
-        install_requires=[
-            "requests>=2.6.0",
-        ],
-    )
-
-
-if __name__ == "__main__":
-    main()
+setup(
+    name='testing.elasticsearch',
+    packages=['testing'],
+    package_dir={'': 'src'},
+    version=open('VERSION.txt').read().strip(),
+    author='Jon Gartman',
+    author_email='jongartman@gmail.com',
+    url='https://github.com/jong/testing.elasticsearch',
+    keywords=['testing', 'elasticsearch'],
+    license='MIT',
+    description='Automatically sets up an elasticsearch instance in a temporary directory, and destroys it after testing',
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "requests>=2.6.0",
+    ],
+)
